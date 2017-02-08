441794a3f48d24b4ef4bbe3f58d8905471c7630f
ACastello <a.castello.pas@gmail.com>
2017-02-08 15:35:51 +0100

    .

--- Main.hs
+++ Main.hs
@@ -1,21 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
 
 import Data.Array
-import Data.List
+import Data.List 
+
 import System.Console.Terminal.Size
+import System.Console.Terminfo
 
-data RT a 
 
-box :: Array (Int,Int) String -> Int -> Int -> Int -> Int -> Array (Int, Int) String
-box arr x y x' y' = arr // ([((x,y), col [0,1] ++ "┌"), ((x',y), "┐" ++ col [])
-    , ((x,y'), col [0,1] ++ "└"), ((x',y'), "┘" ++ col [])] ++
-    [((j,y''), "─") | j <- [x+1..x'-1], y'' <- [y,y']] ++
-    [((x'',j), col [1] ++ "│" ++ col []) | j <- [y+1..y'-1], x'' <- [x,x']]) 
-col [] = col [0]
-col xs = "\ESC[" ++ foldl1 (\s s' -> s ++ ";" ++ s') (show <$> xs) ++ "m\STX"
+data Term = Term
+    { mv    :: Int -> Int -> RT ()
+    , dims  :: RT (Int,Int)
+    , bold  :: RT ()
+    , dim   :: RT ()
+    , reset :: RT ()
+    }
 
--- p :: Array (Int, Int) String -> IO ()
-p arr = foldMap putStr $ do
-    j <- [ly..uy]
-    return $ concat [arr ! (i,j) | i <- [lx..ux]] ++ "\n"
+type RT = ReaderT Term IO
+
+pc :: Char -> RT ()
+pc = liftIO . putChar
+
+runRT :: RT () -> IO ()
+runRT op = do
+    term <- setupTermFromEnv
+    let cap c = maybe undefined (liftIO . runTermOutput term) (getCapability term c)
+        dims = liftIO $ do
+            w <- maybe (error "couldn't get terminal size") id <$> size
+            return (height w, width w)
+        act output y x = liftIO $ runTermOutput term (output (Point y x))
+        mv = maybe (error "moving the cursor is not supported in this terminal") 
+                act (getCapability term cursorAddress)
+    runReaderT op (Term mv dims (cap boldOn) (cap dimOn) (cap allAttributesOff)) where
+
+drawComplete :: Int -> Int -> [Int] -> RT ()
+drawComplete y x zs = do
+    drawLines 0 0 y x
+    drawBox 0 0 y x
+    drawList (y-1) (x+1) zs
+
+drawList :: Int -> Int -> [Int] -> RT ()
+drawList y x zs = do
+    t <- ask
+    let f j n = do
+        (mv t) (y-n) j
+        pc '∙'
+    sequence_ $ zipWith f [x..] zs
+
+drawBox :: Int -> Int -> Int -> Int -> RT ()
+drawBox yo xo yf xf = do
+    t @ Term { mv = mv } <- ask
+    bold t
+    mv yo xo
+    liftIO $ putChar '┌'
+    mv yo xf
+    liftIO $ putChar '┐'
+    mv yf xo
+    liftIO $ putChar '└'
+    mv yf xf
+    liftIO $ putChar '┘'
+    forM_ [xo+1..xf-1] $ \j -> do
+        mv yo j
+        pc '─'
+        mv yf j
+        pc '─'
+    forM_ [yo+1..yf-1] $ \i -> do
+        mv i xo
+        pc '│'
+        mv i xf
+        pc '│'
+    reset t
+
+drawLine :: Int -> Int -> Int -> RT ()
+drawLine y xo xf = do
+    t <- ask
+    forM_ [xo..xf] $ \j -> do
+        (mv t) y j
+        pc '─'
+
+drawLines :: Int -> Int -> Int -> Int -> RT ()
+drawLines yo xo yf xf = do
+    t <- ask
+    dim t
+    forM_ [yf-(step-1)-step*y | y <- if yr < (step+2) then [] else [0..yr`quot`step]] $ \i -> do
+        drawLine i xo xf
+    reset t
     where
-        ((lx, ly), (ux,uy)) = bounds arr
+        yr = yf-yo
+        step = 3
