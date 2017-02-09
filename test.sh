2b9af740191947b07e8d7afe22b06927bffbb590
ACastello <a.castello.pas@gmail.com>
2017-02-09 17:05:38 +0100

    last working Main with Terminfo

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-ping -i 0.3 192.168.1.1 | sed -u 's/.*time=\(.*\)ms/\1/g' | grep -v --line-buffered PING
+if [ "$#" -gt 0 ] ; then
+    dom="$1"
+else
+    dom="192.168.1.1"
+fi
+
+ping -i 0.3 "$dom" | sed -u 's/.*time=\(.*\)ms/\1/g' | grep -v --line-buffered PING
